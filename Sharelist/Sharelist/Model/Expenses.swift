14bcbf4167191205400befddb0bc9b39b144358d
melinaosterloh <melina_osterloh25@web.de>
2023-07-04 23:10:34 +0200

    PaymentTableView

---
+++ Sharelist/Sharelist/Model/Expenses.swift
@@ -0,0 +1,17 @@
+//
+//  Expenses.swift
+//  Sharelist
+//
+//  Created by Melina Osterloh on 04.07.23.
+//
+
+import Foundation
+
+struct Expenses:Identifiable {
+    
+    var id: String
+    var date: String
+    var description: String
+    var price: Double
+    
+}
