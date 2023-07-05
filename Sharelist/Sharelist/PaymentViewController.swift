d2d6ec8467dbac43601f6924e2673623d57a3eb2
melinaosterloh <melina_osterloh25@web.de>
2023-07-05 13:00:09 +0200

    Expanses Table View

--- Sharelist/Sharelist/PaymentViewController.swift
+++ Sharelist/Sharelist/PaymentViewController.swift
@@ -8,22 +8,91 @@
 import UIKit
 import Firebase
 
-class PaymentViewController: UIViewController {
+class PaymentViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var payDeptBtn: UIButton!
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var homeBtn: UIButton!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var paymentTableView: UITableView!
+    
     
     @IBOutlet weak var balance: UILabel!
     var userID = Auth.auth().currentUser!.uid
     
+    var expensesArray = [Expenses]()
+    var expenses: Expenses?
+    private var document: [DocumentSnapshot] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.paymentTableView.delegate = self
+        self.paymentTableView.dataSource = self
+        
+        loadData()
         loadDesign()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return expensesArray.count
+    }
     
+    // Table View Cell Höhe
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let expensesCell = tableView.dequeueReusableCell(withIdentifier: "ExpensesTableViewCell", for: indexPath)
+        let expenses = expensesArray[indexPath.row]
+        let number = expenses.price
+        let string = "\(number)"
+
+        expensesCell.textLabel?.text = "\(expenses.date + "    " + expenses.description)"
+        expensesCell.detailTextLabel?.text = "\(string + " €")"
+        
+        expensesCell.layer.cornerRadius = 10
+        expensesCell.layer.borderColor = UIColor.darkGray.cgColor
+        expensesCell.layer.borderWidth = 0.3
+        
+        return expensesCell
+    }
+    
+    func loadData() {
+        
+        // Vor dem Laden neuer Daten das listArray leeren
+        expensesArray.removeAll()
+        
+        let db = Firestore.firestore()
+        //initalize Database
+
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+           let selectedListID = appDelegate.selectedListID {
+                print("In der Sharelist ist diese ID angekommen:", selectedListID)
+                let shoppingListsRef = db.collection("shoppinglist").document(selectedListID).collection("expenses")
+                shoppingListsRef.getDocuments { (snapshot, error) in
+                    if let error = error {
+                        print(error.localizedDescription)
+                    } else {
+                        if let snapshot = snapshot {
+                            for document in snapshot.documents {
+                                let data = document.data()
+                                let id = data["id"] as? String ?? ""
+                                let date = data["date"] as? String ?? ""
+                                let description = data["description"] as? String ?? ""
+                                let price = data["price"] as? Double ?? 0
+                                let newExpenses = Expenses(id: id, date: date, description: description, price: price)
+                                self.expensesArray.append(newExpenses)
+                            }
+                            self.paymentTableView.reloadData()
+                        }
+                    }
+                }
+            } else {
+                print("User ist null")
+            }
+    }
 
     @IBAction func menuButton(_ sender: UIButton) {
         let accountViewController = storyboard?.instantiateViewController(withIdentifier: "AccountViewController") as! AccountViewController
@@ -73,6 +142,12 @@ class PaymentViewController: UIViewController {
         homeBtn.layer.shadowOpacity = 0.5
         homeBtn.layer.shadowColor = UIColor.darkGray.cgColor
         homeBtn.layer.shadowOffset = CGSize(width: 1, height: 1)
+        
+        addBtn.layer.cornerRadius = addBtn.bounds.height / 2
+        addBtn.layer.shadowRadius = 2
+        addBtn.layer.shadowOpacity = 0.5
+        addBtn.layer.shadowColor = UIColor.darkGray.cgColor
+        addBtn.layer.shadowOffset = CGSize(width: 1, height: 1)
         
     }
     
